988f9ffd906c26dc285ce22973dcc95538882bc9
Aftar Fadilah <aftar.fadilah@nosc.ai>
2023-09-23 19:15:08 +0700

    homepage init

---
+++ types/generated/components.d.ts
@@ -0,0 +1,47 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface TampilanBanner extends Schema.Component {
+  collectionName: 'components_tampilan_banners';
+  info: {
+    displayName: 'banner';
+  };
+  attributes: {
+    judul: Attribute.String;
+    deskripsi: Attribute.Text;
+    link_teks: Attribute.String;
+    link_url: Attribute.String;
+  };
+}
+
+export interface TampilanProdukUnggulan extends Schema.Component {
+  collectionName: 'components_tampilan_produk_unggulans';
+  info: {
+    displayName: 'produk_unggulan';
+    icon: 'apps';
+  };
+  attributes: {
+    foto_produk: Attribute.Media;
+    nama_produk: Attribute.String;
+  };
+}
+
+export interface TampilanSmallCard extends Schema.Component {
+  collectionName: 'components_tampilan_small_cards';
+  info: {
+    displayName: 'small-card';
+    icon: 'bulletList';
+  };
+  attributes: {
+    isi: Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Shared {
+    export interface Components {
+      'tampilan.banner': TampilanBanner;
+      'tampilan.produk-unggulan': TampilanProdukUnggulan;
+      'tampilan.small-card': TampilanSmallCard;
+    }
+  }
+}
